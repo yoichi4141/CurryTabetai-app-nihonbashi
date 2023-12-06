ebee51f380bca8ca9d6983373920148e818bce77
kentaro kimoto <exiler2411k@gmail.com>
2023-12-06 10:18:29 +0900

    画面構成

---
+++ lib/src/screens/timeline/timeline.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TimelineScreen extends StatelessWidget {
+  const TimelineScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('タイムライン'),
+      ),
+      body:
+          const Center(child: Text('タイムライン', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
