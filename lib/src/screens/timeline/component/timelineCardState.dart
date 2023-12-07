e13605ffe6e4da0a0c55f573c0cdc026a3a2efe8
yoichi1 <>
2023-12-07 13:51:42 +0900

    タイムラインUIですね〜

---
+++ lib/src/screens/timeline/component/timelineCardState.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:currytabetaiappnihonbashi/src/screens/timeline/viewModel/timelineViewModel.dart';
+
+class TimelineCard extends StatefulWidget {
+  final TimelineItem item;
+  final String image; // ここに画像を追加
+
+  const TimelineCard({
+    required this.item,
+    required this.image, // コンストラクターに画像を追加
+    // ... other parameters
+  });
+
+  @override
+  _TimelineCardState createState() => _TimelineCardState();
+}
+
+class _TimelineCardState extends State<TimelineCard> {
+  bool favorite = false;
+  bool retweeted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                      // Display user avatar or default avatar
+                      ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.item.userName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          width: 300,
+                          child: Text(
+                            widget.item.text,
+                            softWrap: true,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 300,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: widget.item.image != null
+                                ? DecorationImage(
+                                    image: NetworkImage(widget.item.image!),
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: widget.item.image == null
+                              ? Icon(Icons.add)
+                              : null, // デフォルト表示用のアイコンやウィジェット
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
