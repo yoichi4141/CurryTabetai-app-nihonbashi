1ece3d2256d38c3c58ce755c029a09269ad9278d
yoichi1 <>
2023-12-14 10:28:00 +0900

    投稿画面UI

--- lib/src/screens/post/post.dart
+++ lib/src/screens/post/post.dart
@@ -13,64 +13,10 @@ class PostScreen extends StatelessWidget {
     bool isLogin = random.nextBool();
 
     return Scaffold(
-        appBar: AppBar(title: const Text(''), actions: [
-          IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              _showSettingModal(context);
-            },
-          ),
-        ]),
+
         // ログイン済みだったらsignedProfileView、してなかったらguestpostview
-        body: isLogin ? const SignedpostsearchView() : const SignedpostsearchView());
+        body: isLogin
+            ? const SignedpostsearchView()
+            : const SignedpostsearchView());
   }
-
-// 設定画面モーダルを出す関数
-  void _showSettingModal(BuildContext context) {
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return SizedBox(
-          height: 200,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  // TODO:　リンクは後で更新
-                  _buildButtonWithWebView(context, Icons.help, 'お問い合わせ',
-                      'https://example.com/music'),
-                  _buildButtonWithWebView(context, Icons.safety_check,
-                      'プライバシーポリシー', 'https://example.com/photos'),
-                  _buildButtonWithWebView(context, Icons.how_to_reg, 'ご利用方法',
-                      'https://example.com/video'),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildButtonWithWebView(
-      BuildContext context, IconData icon, String title, String url) {
-    return GestureDetector(
-      onTap: () {
-        // TODO: WebViewを開く処理
-        // https://pub.dev/packages/webview_flutter
-        print('Open WebView for $title: $url');
-      },
-      child: Container(
-        color: Colors.transparent,
-        child: ListTile(
-          leading: Icon(icon),
-          title: Text(title),
-        ),
-      ),
-    );
-  }
-
-  signedpostView() {}
 }
--- lib/src/screens/post/view/posttextfield.dart
+++ lib/src/screens/post/view/posttextfield.dart
@@ -1,4 +1,8 @@
+import 'dart:io'; //File? image;を追加した時に必要になった非Webアプリケーションのファイル、ソケット、HTTP、およびその他のI/Oサポート。
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //PlatformExceptionで必要
+import 'package:intl/intl.dart'; //カレンダーのパッケージ
+import 'package:image_picker/image_picker.dart';
 
 class Posttextfield extends StatefulWidget {
   const Posttextfield({Key? key}) : super(key: key);
@@ -10,108 +14,192 @@ class Posttextfield extends StatefulWidget {
 class _Posttextfield extends State<Posttextfield> {
   final TextEditingController _textEditingController = TextEditingController();
   final int maxCharacters = 500; // 最大文字数
+  late DateTime selectedDate = DateTime.now(); // 選択された日付を保持する変数
+  File? image; //取得したImageのパスが入ってる
+
+//カレンダーの関数(showDatePicker=カレンダー)
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2022),
+      lastDate: DateTime(2025),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            colorScheme: ColorScheme.light().copyWith(
+              primary: Colors.green,
+            ),
+            // カレンダーの背景色を設定
+            dialogBackgroundColor: Colors.white,
+          ),
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+// 画像をギャラリーから選ぶ関数
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      // 画像がnullの場合戻る
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+//カレンダー何も選択肢なかったら今の日付を選択したら選択した日付を
+    String buttonText =
+        '選択した日付: ${selectedDate.year}/${selectedDate.month}/${selectedDate.day}';
+    if (selectedDate == DateTime.now()) {
+      final DateFormat formatter = DateFormat('yyyy/MM/dd');
+      buttonText = 'Today: ${formatter.format(selectedDate)}';
+    } else {
+      buttonText =
+          '${selectedDate.year}/${selectedDate.month}/${selectedDate.day}';
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('カリーログ投稿🍛'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.green, // ボタンの背景色を緑に設定
-                padding: EdgeInsets.symmetric(horizontal: 16.0), // パディングを追加
-              ),
-              child: Text(
-                '投稿する',
-                style: TextStyle(color: Colors.white), // テキストの色を白に設定
+        appBar: AppBar(
+          title: const Text(
+            'カリーログ投稿🍛',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, // ボタンの背景色を緑に設定
+                  padding: EdgeInsets.symmetric(horizontal: 16.0), // パディングを追加
+                ),
+                child: Text(
+                  '投稿する',
+                  style: TextStyle(color: Colors.white), // テキストの色を白に設定
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 8.0),
-          //リストから引き継いでくる形にする
-          Text(
-            "Vashon/curry&grill",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Row(
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: 120, // 幅を調整
-                height: 40, // 高さを調整
-                child: TextButton(
-                  onPressed: () {
-                    // ボタンがタップされた時の処理
-                  },
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8), // 角丸を設定
-                    ),
-                  ),
-                  child: Text(
-                    '2023/12/10',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
+              SizedBox(height: 0.0),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 10, right: 20, bottom: 0, left: 20),
+                child: Text(
+                  "Vashon/curry&grill",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 5, right: 20, bottom: 0, left: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 30,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await _selectDate(context);
+                          setState(() {});
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text(
+                          ' ${selectedDate.year}/${selectedDate.month}/${selectedDate.day}',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 4.0),
               Container(
-                width: 120, // 幅を調整
-                height: 40, // 高さを調整
-                child: TextButton(
-                  onPressed: () {
-                    // ボタンがタップされた時の処理
-                  },
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8), // 角丸を設定
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.0, color: Colors.white),
+                  borderRadius: BorderRadius.circular(0.0),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(top: 0, right: 20, bottom: 0, left: 20),
+                  child: TextField(
+                    controller: _textEditingController,
+                    maxLength: maxCharacters,
+                    decoration: InputDecoration(
+                      hintText:
+                          '今日食べたカリーの味についてや、お店の雰囲気など、カリーの口コミ・感想を書いてみよう。空の状態で投稿すると「チェックイン」表示になります。',
+                      border: InputBorder.none,
                     ),
+                    maxLines: 8,
+                    keyboardType: TextInputType.multiline,
                   ),
-                  child: Text(
-                    '公開or非公開',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 0, right: 20, bottom: 0, left: 20),
+                child: Column(
+                  children: [
+                    // ... 他のウィジェット
+                    SizedBox(height: 0),
+                    image != null
+                        ? Image.file(
+                            image!,
+                            width: 600, // 画像の幅を設定
+                            height: 500, // 画像の高さを設定
+                          )
+                        : Text(""),
+                  ],
                 ),
               ),
             ],
           ),
+        ),
+        bottomNavigationBar: Divider(height: 16.0, color: Colors.white),
+        persistentFooterButtons: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0), // パディングの設定
 
-          SizedBox(height: 8.0),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.0, color: Colors.grey),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(24.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: InputDecoration(
-                  hintText:
-                      '今日食べたカリーの味についてや、お店の雰囲気など、カリーの口コミ・感想を書いてみよう。空の状態で投稿すると「チェックイン」表示になります。',
-                  border: InputBorder.none,
-                ),
-                maxLines: 10,
-                keyboardType: TextInputType.multiline,
+            child: Row(children: [
+              IconButton(
+                icon: Icon(Icons.image),
+                iconSize: 40,
+                onPressed: () {
+                  pickImage();
+                },
+                visualDensity: VisualDensity.compact,
               ),
-            ),
-          ),
-        ],
-      ),
-    );
+            ]),
+          )
+        ]);
   }
 }
